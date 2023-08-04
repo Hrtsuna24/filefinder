e2d7ee2fad9901e478328dd427df36f1f1581f90
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-08-04 15:18:16 +0300

    11

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,78 @@
-#include <threada>
+#include <thread>
 #include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <future>
+
+namespace HTS
+{
+	using std::thread;
+	using std::vector;
+	using std::cout;
+	using std::promise;
+	using std::string;
+	using std::future;
+}
+
+void fun(std::promise<std::string>&& pr)
+{
+	std::cout << "Worker throwing \n";
+	//pr.set_value("Message from thread");
+	try
+	{
+		throw std::exception("exception");
+	}
+	catch (...)
+	{
+		pr.set_exception(std::current_exception());
+	}
+}
+
+std::string fun(bool flag)
+{
+	if (flag)
+	{
+		return "Message";
+	}
+	else
+	{
+		throw std::exception("aaaaaa");
+		return "Message";
+	}
+}
 
 void main()
 {
+	using namespace HTS;
 
+	/*promise<string> pr;
+	future<string> fut = pr.get_future();
+	thread th(fun, std::move(pr));
+
+	cout << "Main recived: \n";
+	try
+	{
+		cout << fut.get();
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what();
+	}
+	th.join();*/
+
+
+	/// <summary>
+	/// ////////////////////////////////
+	/// </summary>
+	future<string> fut = std::async(fun, true);
+	cout << "Main recived: \n";
+	try
+	{
+		cout << fut.get();
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what();
+	}
 }
